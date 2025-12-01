0d44e51e9f66ff0f374fb797c57a348129667ea8
srikanta <srikantabhoi196@gmail.com>
2025-12-01 04:03:02 +0000

    part 7 and part8

---
+++ part7/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const fs = require("fs");
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  fs.readdir(`./files`, (err, files) => {
+    res.render("index", { files: files }); //sending files to frontend
+  });
+});
+app.get("/file/:filename", (req, res) => {
+  fs.readFile(`./files/${req.params.filename}`, "utf-8", (err, data) => {
+    if (err) console.log(err);
+    else {
+      res.render("show", { filename: req.params.filename, filedata: data });
+    }
+  });
+});
+
+app.post("/create", (req, res) => {
+  // console.log(req.body)
+  fs.writeFile(
+    `./files/${req.body.title.split(" ").join("")}.txt`,
+    req.body.details,
+    (err) => {
+      if (err) console.log(err);
+      res.redirect("/");
+    }
+  );
+});
+
+app.listen(3000);
---
+++ part7/redme.md
@@ -0,0 +1,3 @@
+# Project 
+
+## Note taking web app
